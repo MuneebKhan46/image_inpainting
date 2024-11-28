af21c4f193128ea0fd5c5e881531b39c304b46d1
Muneeb A. Khan <81796045+MuneebKhan46@users.noreply.github.com>
2024-11-28 23:56:57 +0500

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,382 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+get_ipython().system('nvidia-smi')
+
+
+# In[1]:
+
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+from skimage.metrics import peak_signal_noise_ratio as compare_psnr
+from skimage.metrics import structural_similarity as compare_ssim
+
+
+# In[2]:
+
+
+class YUVImageDataset(Dataset):
+    def __init__(self, yuv_dir, diff_map_dir, image_size=(720, 1280), mask_threshold=0.1, mode='train', triplets_list=None):
+        self.yuv_dir = yuv_dir
+        self.diff_map_dir = diff_map_dir
+        self.image_size = image_size
+        self.mask_threshold = mask_threshold
+        self.mode = mode
+
+        if triplets_list is None:
+            self.image_triplets = self._load_image_triplets()
+        else:
+            self.image_triplets = triplets_list
+        print(f"Loaded {len(self.image_triplets)} image triplets for mode {self.mode}.")
+
+    def _load_image_triplets(self):
+        image_triplets = []
+        for filename in os.listdir(self.yuv_dir):
+            if filename.startswith('original_') and filename.endswith('.raw'):
+                base_name = filename[len('original_'):-len('.raw')]
+                original_path = os.path.join(self.yuv_dir, filename)
+                denoised_filename = f'denoised_{base_name}.raw'
+                denoised_path = os.path.join(self.yuv_dir, denoised_filename)
+                diff_map_filename = f'difference_{base_name}.png'
+                diff_map_path = os.path.join(self.diff_map_dir, diff_map_filename)
+
+                if os.path.exists(denoised_path) and os.path.exists(diff_map_path):
+                    image_triplets.append((original_path, denoised_path, diff_map_path))
+                else:
+                    print(f"Missing files for base name {base_name}")
+        return image_triplets
+
+    def load_y_channel(self, yuv_path):
+        width, height = self.image_size
+        y_size = width * height
+        with open(yuv_path, 'rb') as f:
+            y_channel = np.frombuffer(f.read(y_size), dtype=np.uint8).reshape((height, width))
+        return y_channel
+
+    def load_diff_map(self, diff_map_path):
+        diff_map = Image.open(diff_map_path).convert('L')
+        diff_map = diff_map.resize(self.image_size)
+        return np.array(diff_map)
+
+    def generate_mask_from_diff_map(self, diff_map):
+        diff_map = diff_map / 255.0
+        mask = np.where(diff_map > self.mask_threshold, 0, 1).astype(np.uint8)
+        return mask
+
+    def __len__(self):
+        return len(self.image_triplets)
+    
+    def __getitem__(self, idx):
+        original_path, denoised_path, diff_map_path = self.image_triplets[idx]
+
+
+        if self.mode == 'train':
+            input_image_path = original_path
+        elif self.mode == 'test':
+            input_image_path = denoised_path
+        else:
+            raise ValueError(f"Invalid mode: {self.mode}")
+
+        y_channel = self.load_y_channel(input_image_path)
+        ground_truth_y_channel = self.load_y_channel(original_path)
+
+        diff_map = self.load_diff_map(diff_map_path)
+        mask = self.generate_mask_from_diff_map(diff_map)
+
+        masked_y_channel = y_channel.copy()
+        masked_y_channel[mask == 0] = 0
+
+        y_tensor = torch.from_numpy(ground_truth_y_channel).unsqueeze(0).float() / 255.0
+        masked_y_tensor = torch.from_numpy(masked_y_channel).unsqueeze(0).float() / 255.0
+        mask_tensor = torch.from_numpy(mask).unsqueeze(0).float()
+
+        return masked_y_tensor, y_tensor, mask_tensor, y_channel
+
+
+
+# In[3]:
+
+
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, 64, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1),
+            nn.ReLU()
+        )
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(64, 1, kernel_size=4, stride=2, padding=1),
+            nn.Sigmoid()
+        )
+        
+    def forward(self, x):
+        enc = self.encoder(x)
+        output = self.decoder(enc)
+        return output
+
+
+# In[4]:
+
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(1, 64, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(512, 1, kernel_size=4, stride=1, padding=1),
+            nn.Sigmoid()
+        )
+        
+    def forward(self, x):
+        return self.model(x)
+
+
+# In[5]:
+
+
+def adversarial_loss(pred, target):
+    return nn.BCELoss()(pred, target)
+
+def reconstruction_loss(pred, target):
+    return nn.L1Loss()(pred, target)
+
+
+# In[6]:
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+generator = Generator().to(device)
+generator = nn.DataParallel(generator) 
+
+discriminator = Discriminator().to(device)
+discriminator = nn.DataParallel(discriminator)
+
+optimizer_G = optim.Adam(generator.parameters(), lr=0.0002, betas=(0.5, 0.999))
+optimizer_D = optim.Adam(discriminator.parameters(), lr=0.0002, betas=(0.5, 0.999))
+
+
+# In[7]:
+
+
+yuv_dir = '/Artifact Removal/Dataset/dataset_full' 
+diff_map_dir = '/Artifact Removal/Dataset/detection_map_full'
+
+
+# In[8]:
+
+
+full_dataset = YUVImageDataset(yuv_dir, diff_map_dir, image_size=(720, 1280), mask_threshold=0.1)
+
+
+# In[9]:
+
+
+dataset_size = len(full_dataset)
+indices = list(range(dataset_size))
+split = int(np.floor(0.25 * dataset_size))
+np.random.shuffle(indices)
+train_indices, test_indices = indices[split:], indices[:split]
+
+
+# In[10]:
+
+
+train_triplets = [full_dataset.image_triplets[i] for i in train_indices]
+test_triplets = [full_dataset.image_triplets[i] for i in test_indices]
+
+
+# In[11]:
+
+
+train_dataset = YUVImageDataset(yuv_dir, diff_map_dir, image_size=(720, 1280), 
+                                mask_threshold=0.1, mode='train', triplets_list=train_triplets)
+
+test_dataset = YUVImageDataset( yuv_dir, diff_map_dir, image_size=(720, 1280), 
+                               mask_threshold=0.1, mode='test', triplets_list=test_triplets)
+
+
+# In[12]:
+
+
+train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False)
+
+
+# In[13]:
+
+
+def train_gan(dataloader, num_epochs=100):
+    for epoch in range(num_epochs):
+        for i, (masked_y, y_channel, mask, _) in enumerate(dataloader):
+            masked_y = masked_y.to(device)
+            y_channel = y_channel.to(device)
+            mask = mask.to(device)
+                        
+            optimizer_D.zero_grad()
+            real_output = discriminator(y_channel)
+            real_labels = torch.ones_like(real_output).to(device)
+            fake_labels = torch.zeros_like(real_output).to(device)
+            d_loss_real = adversarial_loss(real_output, real_labels)
+            
+            generated = generator(masked_y)
+            fake_output = discriminator(generated.detach())
+            d_loss_fake = adversarial_loss(fake_output, fake_labels)
+            
+            d_loss = (d_loss_real + d_loss_fake) / 2
+            d_loss.backward()
+            optimizer_D.step()
+            
+            optimizer_G.zero_grad()
+            fake_output = discriminator(generated)
+            g_adv_loss = adversarial_loss(fake_output, real_labels)
+            g_rec_loss = reconstruction_loss(generated * mask, y_channel * mask)
+            g_loss = g_adv_loss + g_rec_loss
+            g_loss.backward()
+            optimizer_G.step()
+            
+            print(f"Epoch [{epoch+1}/{num_epochs}] Batch [{i+1}/{len(dataloader)}] "
+                  f"D Loss: {d_loss.item():.4f}, G Loss: {g_loss.item():.4f}")
+
+
+        test_model(generator, test_loader)
+
+
+# In[14]:
+
+
+def test_model(generator, test_loader):
+    generator.eval()
+    total_psnr = 0
+    total_ssim = 0
+    count = 0
+    with torch.no_grad():
+        for masked_y, y_channel, mask, _ in test_loader:
+            masked_y = masked_y.to(device)
+            y_channel = y_channel.to(device)
+            generated = generator(masked_y)
+            generated_np = generated.cpu().squeeze(1).numpy()
+            y_channel_np = y_channel.cpu().squeeze(1).numpy()
+            for i in range(generated_np.shape[0]):
+                psnr = compare_psnr(y_channel_np[i], generated_np[i], data_range=1.0)
+                ssim = compare_ssim(y_channel_np[i], generated_np[i], data_range=1.0)
+                total_psnr += psnr
+                total_ssim += ssim
+                count += 1
+    avg_psnr = total_psnr / count
+    avg_ssim = total_ssim / count
+    print(f"Validation - Average PSNR: {avg_psnr:.4f}, Average SSIM: {avg_ssim:.4f}")
+    generator.train()
+
+
+# In[ ]:
+
+
+train_gan(train_loader, num_epochs=100)
+
+
+# In[ ]:
+
+
+def visualize_results(generator, test_loader, num_images=5):
+    generator.eval()
+    with torch.no_grad():
+        for i, (masked_y, y_channel, mask, _) in enumerate(test_loader):
+            masked_y = masked_y.to(device)
+            y_channel = y_channel.to(device)
+            generated = generator(masked_y).cpu().squeeze(1).numpy()
+            masked_y = masked_y.cpu().squeeze(1).numpy()
+            y_channel = y_channel.cpu().squeeze(1).numpy()
+            
+            for j in range(min(len(generated), num_images)):
+                fig, axs = plt.subplots(1, 3, figsize=(18, 6))
+                
+                # Masked Input Image
+                axs[0].imshow(masked_y[j], cmap='gray')
+                axs[0].set_title("Masked Input Image")
+                axs[0].axis('off')
+                
+                # Ground Truth Image
+                axs[1].imshow(y_channel[j], cmap='gray')
+                axs[1].set_title("Ground Truth Image")
+                axs[1].axis('off')
+                
+                # Generated Image
+                axs[2].imshow(generated[j], cmap='gray')
+                axs[2].set_title("Generated Image")
+                axs[2].axis('off')
+                
+                plt.show()
+                
+            if i * len(generated) >= num_images:
+                break
+    generator.train()
+
+
+visualize_results(generator, test_loader, num_images=5)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+print("a")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
